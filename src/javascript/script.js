a6fef6b60838a87fe89ccd3c8fec8bbcdc231856
Isadora <isa_carvalho_12@hotmail.com>
2024-04-23 16:26:53 -0300

    update

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+
+    $('#mobile_btn').on('click', function () {
+        $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find('i').toggleClass('fa-x');
+    });
     const sections = $('section');
     const navItems = $('.nav-item');
 
